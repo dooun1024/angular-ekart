ef88be929a0113bc9263e2463be39e8e3289f85f
dooun1024 <43807838+dooun1024@users.noreply.github.com>
2024-06-04 00:19:43 +0900

    learn +

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,15 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  name = "akiyama";
   product  =  {
-    name : 'iPhone',
+    name : 'iPhone X',
     price : 999,
-    color : 'Red'
+    color : 'Red',
+    inStock : 10,
+    pImage: '/assets/images/iphone.webp'
   }
 
   display = false;
 
+  addToCart = 0;
+
   onClick(){
     this.display = true;
   }
+
+  getDiscountedPrice(){
+    return 10;
+  }
+
+  onNameChange(event:any){
+    this.name = event.target.value;
+  }
+
+  decraceCart(){
+    if (this.addToCart>0){
+      this.addToCart--;
+    }
+  }
+
+  incraceCart(){
+    this.addToCart++;
+  }
+
 }
